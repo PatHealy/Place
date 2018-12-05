e8163de9c647f40d1dd815ee061852a4536bf6d0
PatHealy <pwh5@pitt.edu>
2018-12-05 17:47:51 -0500

    Added support to fill entire panels; DRYed fill pixel

--- Web/Place.py
+++ Web/Place.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 if __name__ == "__main__":
     start_grid = [[0 for x in range(15)] for y in range(15)]
-    start_grid[0][0] = [[[0 for x in range(3)] for y in range(32)] for z in range(32)]
+    start_grid[0][0] = [[[255 for x in range(3)] for y in range(32)] for z in range(32)]
+    start_grid[0][1] = [[[255 for x in range(3)] for y in range(32)] for z in range(32)]
     with open("grid_data", "w") as outFile:
         outFile.write(json.dumps(start_grid))
 
@@ -15,7 +16,7 @@ lock = Lock()
 lock_history = Lock()
 
 @app.route("/")
-def hello():
+def home():
     return render_template("home.html")
 
 @app.route("/spimewrangler")
@@ -40,33 +41,47 @@ def set_shape(shape):
             for y in range(15):
                 if [x,y] in data:
                     if active_grid[y][x] == 0:
-                        active_grid[y][x] = [[[0 for x in range(3)] for y in range(32)] for z in range(32)]
+                        active_grid[y][x] = [[[255 for x in range(3)] for y in range(32)] for z in range(32)]
                 else:
                     active_grid[y][x] = 0
         with open("grid_data", "w") as outFile:
             outFile.write(json.dumps(active_grid))
     return "Good"
+
+@app.route("/set_pixels/<data>", methods=['POST'])
+def set_pixels(data):
+    #sets a pixels to a given color
+    changeData = json.loads(data)
+    historyString = ""
     
-@app.route("/set_pixel/<grid_data>/<coordinate>/<rgb>", methods=['POST'])
-def set_pixel(grid_data, coordinate, rgb):
-    #sets a pixel to a given color
-    #coordinate is a tuple of tuples
-    grid = [int(x) for x in grid_data.split(",")]
-    cord = [int(x) for x in coordinate.split(",")]
     with lock:
         with open("grid_data") as inFile:
             try:
                 active_grid = json.loads(inFile.readline())
-            except:
+            except JSONDecodeError:
                 active_grid = [[0 for x in range(15)] for y in range(15)]
-                active_grid[0][0] = [[[0 for x in range(3)] for y in range(32)] for z in range(32)]
-        active_grid[grid[1]][grid[0]][cord[0]][cord[1]] = [int(x) for x in rgb.split(",")]
+                active_grid[0][0] = [[[255 for x in range(3)] for y in range(32)] for z in range(32)]
+                start_grid[0][1] = [[[255 for x in range(3)] for y in range(32)] for z in range(32)]
+                with open("grid_data", "w") as outFile:
+                    outFile.write(json.dumps(active_grid))
+                print("OOPS")
+                return "Big oops"
+            except:
+                print("oops")
+                return "Little oops"
+        for entry in changeData:
+            try:
+                active_grid[entry[1]][entry[0]][entry[2]][entry[3]] = entry[4]
+                historyString += json.dumps(entry) + "\n"
+            except:
+                print(entry)
         with open("grid_data", "w") as outFile:
             outFile.write(json.dumps(active_grid))
     with lock_history:
         with open("grid_history", "a") as outFile:
-            outFile.write(grid_data + "/" + coordinate + "/" + rgb + "\n")
+            outFile.write(historyString)
     return "Good"
+
 
 @app.route("/get_history")
 def get_history():
@@ -80,3 +95,7 @@ def get_grid():
     with open("grid_data") as inFile:
         active_grid = inFile.readline()
     return active_grid
+
+@app.route("/silicon")
+def get_silicon():
+    return render_template("silicon.html")
