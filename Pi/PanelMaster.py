c23f175cdacccb5ba4f10e83cb2753ecd29a67b8
PatHealy <pwh5@pitt.edu>
2018-12-10 20:00:19 -0500

    Modified pi code to only send necessary pixels

--- Pi/PanelMaster.py
+++ Pi/PanelMaster.py
@@ -18,7 +18,11 @@ panel1 = Panel(bus, 0x04)
 panel2 = Panel(bus, 0x05)
 
 panels = [[panel1,panel2]]
-    
+
+color_grid = [[0 for x in range(15)] for y in range(15)]
+color_grid[0][0] = [[[0 for x in range(3)] for y in range(32)] for z in range(32)]
+color_grid[0][1] = [[[0 for x in range(3)] for y in range(32)] for z in range(32)]
+
 #panel.fill_rect([0,0], [32,32], WHITE_PIXEL)
 
 connector = Connector("http://pathealy.pythonanywhere.com/get_grid")
@@ -34,8 +38,9 @@ while True:
                     grid = data[x][y]
                     for i in range(len(grid)):
                         for j in range(len(grid[i])):
-                            panels[x][y].draw_pixel([j, i], grid[j][i])
-                            time.sleep(0.0001)
+                            if not color_grid[x][y] == grid[j][i]:
+                                panels[x][y].draw_pixel([j, i], grid[j][i])
+                                time.sleep(0.0001)
             except:
                 print(y, " Failed")
                 time.sleep(1)                
