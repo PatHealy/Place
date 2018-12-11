57b17116a4c38b5e4ce5bed633adc47026e7a04f
PatHealy <pwh5@pitt.edu>
2018-12-10 20:05:47 -0500

    Fixed infinite web polling potential issue

--- Pi/PanelMaster.py
+++ Pi/PanelMaster.py
@@ -26,11 +26,12 @@ color_grid[0][1] = [[[0 for x in range(3)] for y in range(32)] for z in range(32
 #panel.fill_rect([0,0], [32,32], WHITE_PIXEL)
 
 connector = Connector("http://pathealy.pythonanywhere.com/get_grid")
-
+ticks = 0
+ticked = False
 
 while True:
     data = connector.get_dump()
-
+    ticked = False
     for x in range(len(panels)):
         for y in range(len(panels[x])):
             try:
@@ -41,8 +42,19 @@ while True:
                             if not color_grid[x][y] == grid[j][i]:
                                 panels[x][y].draw_pixel([j, i], grid[j][i])
                                 time.sleep(0.0001)
+                                ticked = True
+                                ticks = 0
+                                
             except:
                 print(y, " Failed")
                 time.sleep(1)                
-
-    time.sleep(0)
+    if not ticked:
+        ticks = ticks + 1
+        
+    if ticks > 100:
+        time.sleep(60)
+    elif ticks > 10:
+        time.sleep(5)
+    else:
+        time.sleep(1)
+    
