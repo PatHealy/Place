4f517276e1e1633aa076f79597c5f71960721806
FrowntownPitt <ajf93@pitt.edu>
2018-12-13 19:21:22 -0500

    Functional semi-regular update

--- Pi/PanelMaster.py
+++ Pi/PanelMaster.py
@@ -15,9 +15,9 @@ BLACK_PIXEL = [0x0, 0x0, 0x0]
 
 
 panel1 = Panel(bus, 0x04)
-panel2 = Panel(bus, 0x05)
+#panel2 = Panel(bus, 0x05)
 
-panels = [[panel1,panel2]]
+panels = [[panel1]]
 
 color_grid = [[0 for x in range(15)] for y in range(15)]
 color_grid[0][0] = [[[0 for x in range(3)] for y in range(32)] for z in range(32)]
@@ -28,6 +28,7 @@ color_grid[0][1] = [[[0 for x in range(3)] for y in range(32)] for z in range(32
 connector = Connector("http://pathealy.pythonanywhere.com/get_grid")
 ticks = 0
 ticked = False
+wait_flag = False
 
 while True:
     data = connector.get_dump()
@@ -35,23 +36,32 @@ while True:
     for x in range(len(panels)):
         for y in range(len(panels[x])):
             try:
+                if ticks % 5 == 0:
+                    panels[0][0].draw_pixel([0,0], data[0][0][0][0])
                 if(panels[x][y] is not None):
                     grid = data[x][y]
                     for i in range(len(grid)):
                         for j in range(len(grid[i])):
-                            if not color_grid[x][y] == grid[j][i]:
+                            if not list(color_grid[x][y][j][i]) == list(grid[j][i]):
                                 panels[x][y].draw_pixel([j, i], grid[j][i])
-                                color_grid[x][y] = grid[j][i]
+                                if waitFlag:
+                                    time.sleep(6)
+                                    panels[x][y].draw_pixel([j,i], grid[j][i])
+                                    waitFlag = False
+                                color_grid[x][y][j][i] = list(grid[j][i])
                                 time.sleep(0.0001)
                                 ticked = True
                                 ticks = 0
                                 
             except:
                 print(y, " Failed")
-                time.sleep(1)                
+                waitFlag = True
+                color_grid[0][0] = [[[0 for x in range(3)] for y in range(32)] for z in range(32)]
+                color_grid[0][1] = [[[0 for x in range(3)] for y in range(32)] for z in range(32)]
+                time.sleep(6)                
     if not ticked:
+        print("No update")
         ticks = ticks + 1
-        
     if ticks > 100:
         time.sleep(60)
     elif ticks > 10:
